de486d7cb2a6341e789566c32dbf1a33952288a6
w3barsi <darcybalaga@gmail.com>
2025-03-26 16:54:36 +0800

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+  out: "./drizzle",
+  schema: "./src/lib/server/schema/index.ts",
+  breakpoints: true,
+  verbose: true,
+  strict: true,
+  dialect: "turso",
+  casing: "snake_case",
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+    authToken: process.env.DATABASE_AUTH_TOKEN as string,
+  },
+} satisfies Config;

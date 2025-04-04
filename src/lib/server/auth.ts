47d3e36d7db1f4912c28450cc948bee25950163f
w3barsi <darcybalaga@gmail.com>
2025-04-04 17:29:15 +0800

    got auth working

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -16,20 +16,23 @@ export const auth = betterAuth({
       maxAge: 5 * 60, // 5 minutes
     },
   },
+  // https://www.better-auth.com/docs/concepts/users-accounts#account-linking
+  account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ["google", "facebook"],
+    },
+  },
 
   // https://www.better-auth.com/docs/concepts/oauth
   socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    },
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    discord: {
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     },
   },
 
--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -1,8 +1,9 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import type { ComponentProps } from "react";
+import { useState, type ComponentProps } from "react";
 import authClient from "~/lib/auth-client";
 import { Button } from "~/lib/components/ui/button";
 import { cn } from "~/lib/utils";
+import { AUTH_SVG } from "~/utils/svg";
 
 const REDIRECT_URL = "/dashboard";
 
@@ -23,21 +24,8 @@ function AuthPage() {
       <div className="flex flex-col items-center gap-8 rounded-xl border bg-card p-10">
         Logo here
         <div className="flex flex-col gap-2">
-          <SignInButton
-            provider="discord"
-            label="Discord"
-            className="bg-[#5865F2] hover:bg-[#5865F2]/80"
-          />
-          <SignInButton
-            provider="github"
-            label="GitHub"
-            className="bg-neutral-700 hover:bg-neutral-700/80"
-          />
-          <SignInButton
-            provider="google"
-            label="Google"
-            className="bg-[#DB4437] hover:bg-[#DB4437]/80"
-          />
+          <SignInButton provider="google" label="Google" className="" />
+          <SignInButton provider="facebook" label="Facebook" className="" />
         </div>
       </div>
     </div>
@@ -45,25 +33,38 @@ function AuthPage() {
 }
 
 interface SignInButtonProps extends ComponentProps<typeof Button> {
-  provider: "discord" | "google" | "github";
+  provider: "google" | "facebook";
   label: string;
 }
 
 function SignInButton({ provider, label, className, ...props }: SignInButtonProps) {
+  const [clicked, setClicked] = useState(false);
+  const Logo = AUTH_SVG[provider];
+
   return (
     <Button
-      onClick={() =>
+      onClick={() => {
+        setClicked(true);
         authClient.signIn.social({
           provider,
           callbackURL: REDIRECT_URL,
-        })
-      }
+        });
+      }}
+      disabled={clicked}
       type="button"
       size="lg"
-      className={cn("text-white hover:text-white", className)}
+      className={cn(
+        "bg-white hover:bg-neutral-200 text-neutral-900 hover:text-black cursor-pointer",
+        className,
+      )}
       {...props}
     >
-      Sign in with {label}
+      <div className="grid w-full grid-cols-5">
+        <span className="col-span-1 flex items-center">
+          <Logo />
+        </span>
+        <span className="col-span-4">Sign in with {label}</span>
+      </div>
     </Button>
   );
 }

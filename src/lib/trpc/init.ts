4b5ef1b7824b8a8bed2165d8417799ec3150b845
w3barsi <darcybalaga@gmail.com>
2025-04-29 00:14:14 +0800

    pass down db and session to trpc
    a

--- src/lib/trpc/init.ts
+++ src/lib/trpc/init.ts
@@ -1,7 +1,20 @@
 import { initTRPC } from "@trpc/server";
+import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
+import { auth } from "../server/auth";
+import { db } from "../server/db";
 
-const t = initTRPC.create({
+export async function createContext(opts: CreateNextContextOptions) {
+  const session = await auth.api.getSession({ headers: opts.req.headers });
+
+  return {
+    db,
+    session: session?.user || null,
+    opts,
+  };
+}
+
+const t = initTRPC.context<typeof createContext>().create({
   transformer: superjson,
 });
 
--- src/routes/api/trpc.$.ts
+++ src/routes/api/trpc.$.ts
@@ -1,5 +1,6 @@
 import { createAPIFileRoute } from "@tanstack/react-start/api";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { createContext } from "~/lib/trpc/init";
 import { trpcRouter } from "~/lib/trpc/router";
 
 function handler({ request }: { request: Request }) {
@@ -7,6 +8,13 @@ function handler({ request }: { request: Request }) {
     req: request,
     router: trpcRouter,
     endpoint: "/api/trpc",
+    createContext: async (opts) => {
+      return createContext({
+        ...opts,
+        req: request,
+        res: undefined,
+      });
+    },
   });
 }
 

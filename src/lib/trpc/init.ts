09716b8af9071c333c91d6cae835c57402b49880
w3barsi <darcybalaga@gmail.com>
2025-04-30 01:10:56 +0800

    added error formatter. taken from t3-app

--- src/lib/trpc/init.ts
+++ src/lib/trpc/init.ts
@@ -1,6 +1,7 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
+import { ZodError } from "zod";
 import { auth } from "../server/auth";
 import { db } from "../server/db";
 
@@ -16,7 +17,28 @@ export async function createContext(opts: CreateNextContextOptions) {
 
 const t = initTRPC.context<typeof createContext>().create({
   transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
 });
 
 export const createTRPCRouter = t.router;
+
 export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  if (ctx.session === null || ctx.session?.id === undefined) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+      session: { ...ctx.session },
+    },
+  });
+});
